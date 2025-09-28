f4cc0ca324f84f3d2fbc4855ce417ee17cdff0fc
niklasovaska <niklas.ovaska@gmail.com>
2025-09-28 18:43:34 +0300

    api url change

--- src/lib/api/getAllCourses.tsx
+++ src/lib/api/getAllCourses.tsx
@@ -1,9 +1,7 @@
 import { Courses } from "@/types/course.types";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL 
-
 export async function getAllCourses(): Promise<Courses> {
-    const res = await fetch(`${API_URL}/api/courses`)
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL ?? ""}/api/courses`)
 
     if(!res.ok) throw new Error(`Request failed: ${res.status}`)
 
