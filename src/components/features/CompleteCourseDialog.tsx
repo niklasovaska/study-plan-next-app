36badcad06fc4d1dd4b70b00aca1c6309faa52db
niklasovaska <niklas.ovaska@gmail.com>
2025-09-28 16:33:37 +0300

    complete and delete dialogs

--- src/components/features/CompleteCourseDialog.tsx
+++ src/components/features/CompleteCourseDialog.tsx
@@ -1,40 +1,29 @@
 "use client"
 
-import { Dialog, DialogContent, DialogTitle, DialogClose, DialogDescription } from "../ui/dialog"
+import { Dialog, DialogContent, DialogTitle, DialogClose, DialogDescription, DialogHeader, DialogFooter } from "../ui/dialog"
 import { Label } from "../ui/label"
 import { Button } from "../ui/button"
-import { DialogHeader, DialogFooter } from "../ui/dialog"
 import { Input } from "../ui/input"
 import { completeCourse } from "@/lib/actions"
 import { useActionState, useEffect } from "react"
 import { toast } from "sonner"
-import { useCourseContext } from "@/context/CourseContext"
-import { stat } from "fs"
-
-
-type DialogProps = {
-    id: string
-    name: string
-    open: boolean
-    setOpen: (open: boolean) => void
-}
+import {  useQueryClient } from "@tanstack/react-query"
+import { DialogProps } from "@/types/course.types"
 
 const CompleteCourseDialog = ({ id, name, open, setOpen }: DialogProps) => {
+    const queryClient = useQueryClient()
 
     const [state, formAction, isPending] = useActionState(
         completeCourse, { success: false })
-    
-    const { setCourses } = useCourseContext()
-    
+
     useEffect(() => {
-        if (state.success) {
-            if(state.data) {
-                setCourses(state.data.courses)
-            }
+        if(state.success) {
+        queryClient.invalidateQueries({ queryKey: ["upcoming"] })
         setOpen(false)
         toast.success(`${name} completed`)
-        }
+    }
     }, [state.success])
+    
 
     return(
         <Dialog open={open} onOpenChange={setOpen}>
@@ -42,8 +31,8 @@ const CompleteCourseDialog = ({ id, name, open, setOpen }: DialogProps) => {
                 <form action={formAction}>
                     <DialogHeader className="mb-4">
                         <DialogTitle>Complete a course</DialogTitle>
-                        <DialogDescription className="flex flex-col gap-2">
-                            <span>Click save to complete:</span>
+                        <DialogDescription className="flex flex-col gap-4">
+                            <span>Enter grade and click save to complete</span>
                             <span className="font-bold">{name}</span>
                         </DialogDescription>
                     </DialogHeader>
@@ -61,7 +50,7 @@ const CompleteCourseDialog = ({ id, name, open, setOpen }: DialogProps) => {
                             />
                         </div>
                     </div>
-                    <DialogFooter>
+                    <DialogFooter className="mt-4">
                         <DialogClose asChild>
                         <Button variant="outline">Cancel</Button>
                         </DialogClose>
