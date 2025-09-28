6d337a6183f48f82478a429fb481ac2285bec8df
niklasovaska <niklas.ovaska@gmail.com>
2025-09-28 16:58:42 +0300

    isConnected variable

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,13 +1,18 @@
 import mongoose from "mongoose";
 
+let isConnected = false;
+
 const connectToDatabase = async () => {
+    if (isConnected) return
+    
     try {
         const mongoUrl = process.env.MONGODB_URL;
         if (!mongoUrl) {
             throw new Error('MONGO_URL environment variable is not defined')
         }
         await mongoose.connect(mongoUrl);
-        console.log('connected to MongoDB')
+        isConnected = true;
+        
     } catch (error: unknown) {
         if(error instanceof Error) {
            throw new Error('error connecting to MongoDB' + error.message) 
