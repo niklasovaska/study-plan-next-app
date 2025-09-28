79df2cfd458714593427477a71dfc3afda40be33
niklasovaska <niklas.ovaska@gmail.com>
2025-09-28 18:56:45 +0300

    fetch api url

--- src/lib/api/getAllCourses.tsx
+++ src/lib/api/getAllCourses.tsx
@@ -1,7 +1,7 @@
 import { Courses } from "@/types/course.types";
 
 export async function getAllCourses(): Promise<Courses> {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL ?? ""}/api/courses`)
+    const res = await fetch(`${process.env.VERCEL_URL ? "https://" + process.env.VERCEL_URL : "http://localhost:3000"}/api/courses`)
 
     if(!res.ok) throw new Error(`Request failed: ${res.status}`)
 
