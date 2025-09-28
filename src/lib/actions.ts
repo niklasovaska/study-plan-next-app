e04496ba4a0f94bd05ea3066a68d6a82f484d305
niklasovaska <niklas.ovaska@gmail.com>
2025-09-28 16:33:20 +0300

    implementing zod to server actions

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -2,75 +2,69 @@
 
 import connectToDatabase from "./db"
 import Course from "@/models/Course"
+import { createCourseSchema, completeCourseSchema } from "./validation"
+import * as z from "zod"
 import { revalidatePath } from "next/cache"
-import { getUpcomingCourses } from "./api/getUpcomingCourses"
 
 type CourseState = {
     success: boolean
     error?: string
-    data?: Awaited<ReturnType<typeof getUpcomingCourses>>
 }
 
 export async function createCourse(prevState: CourseState, formData: FormData): Promise<CourseState> {
 
     try {
-        const name = formData.get("name") as string
-        const code = formData.get("code") as string
-        const creditsRaw = formData.get("credits") as string
-
-        if(typeof name !== "string"|| name.trim().length < 1) {
-            return { success: false, error: "Course name cannot be empty"}
+        const rawData = {
+            name: formData.get("name"),
+            code: formData.get("code"),
+            credits: formData.get("credits") 
         }
 
-        if(typeof code !== "string"|| code.trim().length < 1) {
-            return { success: false, error: "Course code cannot be empty"}
-        }
-
-        let credits: number | null = null
-
-        if(typeof creditsRaw === "string" && code.trim() !== "") {
-            credits = Number(creditsRaw)
-            if(isNaN(credits)) {
-                return { success: false, error: "Credits must be a number"}
-            }
-        }
+        const parsedData = createCourseSchema.parse(rawData)
 
         await connectToDatabase()
-        await Course.create({
-            name: name.trim(),
-            code: code.trim(),
-            credits: credits,
+        await Course.create({ ...parsedData,
             status: "upcoming"
-        })
+         })
 
-        const latestData = await getUpcomingCourses()
+        revalidatePath("/")
 
-        return { success: true, data: latestData }
+        return { success: true }
 
     } catch (error: unknown) {
-        if(error instanceof(Error)) {
-            console.log('Error occured', error.message)
+        if(error instanceof z.ZodError) {
+            console.log('Error occured', error.issues)
+            return { success: false, error: (error as any).issues }
         }
+        console.log('Error occured', (error as Error).message)
         return { success: false, error: "Error in adding course to database"}
     }
 }
 
-export async function updateCourseStatus(courseId: string, newStatus: string): Promise<Boolean> {
+export async function updateCourseStatus(courseId: string, newStatus: string) {
     try {
         await connectToDatabase()
         await Course.findByIdAndUpdate(courseId, { status: newStatus })
-        revalidatePath("/upcoming")
-        revalidatePath("/")
 
-        return true
+        revalidatePath("/")
+        
+    } catch (error: unknown) {
+        if(error instanceof(Error)) {
+            console.log('Error occured', error.message)    
+        }
+    }   
+}
+
+export async function deleteCourse(courseId: string) {
+    try {
+        await connectToDatabase()
+        await Course.findByIdAndDelete(courseId)
 
     } catch (error: unknown) {
         if(error instanceof(Error)) {
             console.log('Error occured', error.message)    
         }
-
-        return false
-    }   
+    }
 }
 
 export async function completeCourse(
@@ -78,27 +72,26 @@ export async function completeCourse(
     formData: FormData): Promise<CourseState> {
     
     try {
-        const newGrade = formData.get("grade") as string
-        const id = formData.get("id") as string
-
-        if(typeof newGrade !== "string"|| newGrade.trim().length < 1) {
-            return { success: false, error: "Grade cannot be empty"}
+        const rawData = {
+            courseId: formData.get("id"),
+            grade: formData.get("grade")
         }
 
+        const parsedData = completeCourseSchema.parse(rawData)
+
         await connectToDatabase()
-        await Course.findByIdAndUpdate(id, { 
-            grade: newGrade,
+        await Course.findByIdAndUpdate(parsedData.courseId, { 
+            grade: parsedData.grade,
             status: "completed"})
-        
-        const latestData = await getUpcomingCourses()
 
         revalidatePath("/")
 
-        return { success: true, data: latestData }
+        return { success: true }
 
     } catch (error: unknown) {
-        if(error instanceof(Error)) {
-            console.log('Error occured', error.message)    
+        if(error instanceof(z.ZodError)) {
+            console.log('Error occured', error.issues)
+            return { success: false, error: (error as any).issues }   
         }
 
         return { success: false, error: "Error in completing course"}
